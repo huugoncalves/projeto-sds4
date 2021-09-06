bce102fa167623f0f9f8bcb3299943e6286b7236
Hugo Gonçalves <huugoncalves@gmail.com>
2021-09-06 10:27:48 -0300

    Project created

---
+++ backend/src/test/java/com/huugoncalves/dsvendas/DsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huugoncalves.dsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
